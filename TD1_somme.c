29a8d98ebe9af34f0dd30de6cfc15121332939a6
benzhu <benjamin1002@hotmail.fr>
2021-06-02 11:32:32 +0200

    commit

---
+++ TD1_somme.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void Somme(int t1[], int t2[], int length, int res[]) {
+    for (int i=0; i<length ; i++) {
+        res[i] = t1[i] + t2[i];
+
+    }
+}
+
+
+int main() {
+    int length = 3;
+    int res[length];
+    int t1[3] = {1,2,3};
+    int t2[3] = {4,5,6};
+ 
+
+    Somme(t1,t2,length, res) ;
+    for (int i=0 ; i < length  ; i++) {
+        printf("res[i] = %d\n", res[i]);
+    }
+}
+
